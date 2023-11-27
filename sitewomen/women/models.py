8dd061e0b0344711c65bc292aa84aeee9156e5a4
AlexTrav <nerezov03@mail.ru>
2023-11-27 23:12:44 +0600

    Перенёс ф.п. на к.п. в users. Поменял форму: RegisterUserForm в users.

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -71,7 +71,7 @@ class Category(models.Model):
 
     class Meta:
         verbose_name = 'Категория'
-        verbose_name_plural = 'Катиегории'
+        verbose_name_plural = 'Категории'
 
     def __str__(self):
         return self.name
